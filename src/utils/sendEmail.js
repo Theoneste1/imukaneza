05b3df36435887b03122d21c8e319e991f0fa46a
habaumugisha1 <52486783+habaumugisha1@users.noreply.github.com>
2021-12-28 15:29:38 +0200

    change link for front end
    change link for front end

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -15,7 +15,7 @@ export const sendingVerificationEMail = (email) => {
     subject:"Account verification!",
     html:`
     <h2>Dear ${email},</h2>
-    <p> click here to verify your account ${process.env.HOST}/api/v1/users/verify/${token} </p>
+    <p> click here to verify your account ${process.env.HOST}/users/verify/users/${token} </p>
     `
   }
   return sgMail.send(message)
